5436b141956d531b9fddc8578ad7d93986e00153
ericjair <ericjairlome@gmail.com>
2022-10-12 11:52:51 -0500

    Actualizacion de practicas

---
+++ practica05/Objeto.js
@@ -0,0 +1,17 @@
+var o ={
+    prop:"existe"
+}
+
+var o2 = new Object()
+o2.prop = "existo"
+
+var mensajes_bot = {
+    saludo: "Hola",
+    despedida: "adios",
+    preocupacion: "como estas",
+    sorpresa: "orale",
+}
+
+var arr_msj = ("Hola","adios", "como estas", "orale")
+
+console.log ()
